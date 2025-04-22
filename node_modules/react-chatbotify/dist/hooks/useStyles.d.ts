7db84444dd3141e5dcbc5d04a59d753f335e0ee3
eligulley1 <eligulley@gmail.com>
2025-04-22 11:14:55 -0500

    Delete node_modules directory

--- node_modules/react-chatbotify/dist/hooks/useStyles.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * External custom hook for managing styles.
- */
-export declare const useStyles: () => {
-    styles: import("..").Styles;
-    replaceStyles: (newStyles: import("..").Styles) => void;
-    updateStyles: (fields: import("..").Styles) => void;
-};
-//# sourceMappingURL=useStyles.d.ts.map
