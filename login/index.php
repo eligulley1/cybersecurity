7e9b61e9825e3607fffbac34bd3b7347e1c178af
eligulley1 <eligulley@gmail.com>
2025-04-23 13:21:10 -0500

    Update index.php

--- login/index.php
+++ login/index.php
@@ -13,9 +13,9 @@
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
 
-        <label>Enter CAPTCHA:</label><br>
+<!--         <label>Enter CAPTCHA:</label><br>
         <img src="captcha.php" alt="CAPTCHA"><br>
-        <input type="text" name="captcha" required><br><br>
+        <input type="text" name="captcha" required><br><br> -->
 
         <input type="submit" value="Sign In">
     </form>
