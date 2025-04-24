b3da44f20be2050c369f7c2416c0ed69be888558
eligulley1 <eligulley@gmail.com>
2025-04-24 09:40:28 -0500

    Update index.php

--- login/index.php
+++ login/index.php
@@ -12,15 +12,6 @@
 
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
-
-        <form method="post" action="verify.php">
-            <?php
-              require_once('recaptchalib.php');
-              $publickey = "6LccMCMrAAAAAMzo1mC9zvQfgmm6AOhiaO1Pfo8p"; // you got this from the signup page
-              echo recaptcha_get_html($publickey);
-            ?>
-            <input type="submit" />
-        </form>
         
         <input type="submit" value="Sign In">
     </form>
